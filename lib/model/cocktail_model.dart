a15757f70e7f2eff51ccf92833eaf72d3b345522
DoomHopes <63532660+DoomHopes@users.noreply.github.com>
2021-09-21 11:36:57 +0300

    add cocktail model

---
+++ lib/model/cocktail_model.dart
@@ -0,0 +1,119 @@
+class CocktailModel {
+  CocktailModel({
+    this.idDrink,
+    this.strDrink,
+    this.strCategory,
+    this.strAlcoholic,
+    this.strGlass,
+    this.strInstructions,
+    this.strDrinkThumb,
+    this.strIngredient1,
+    this.strIngredient2,
+    this.strIngredient3,
+    this.strIngredient4,
+    this.strIngredient5,
+    this.strIngredient6,
+    this.strIngredient7,
+    this.strIngredient8,
+    this.strIngredient9,
+    this.strIngredient10,
+    this.strIngredient11,
+    this.strIngredient12,
+    this.strIngredient13,
+    this.strIngredient14,
+    this.strIngredient15,
+    this.strMeasure1,
+    this.strMeasure2,
+    this.strMeasure3,
+    this.strMeasure4,
+    this.strMeasure5,
+    this.strMeasure6,
+    this.strMeasure7,
+    this.strMeasure8,
+    this.strMeasure9,
+    this.strMeasure10,
+    this.strMeasure11,
+    this.strMeasure12,
+    this.strMeasure13,
+    this.strMeasure14,
+    this.strMeasure15,
+  });
+
+  dynamic idDrink;
+  dynamic strDrink;
+  dynamic strCategory;
+  dynamic strAlcoholic;
+  dynamic strGlass;
+  dynamic strInstructions;
+  dynamic strDrinkThumb;
+  dynamic strIngredient1;
+  dynamic strIngredient2;
+  dynamic strIngredient3;
+  dynamic strIngredient4;
+  dynamic strIngredient5;
+  dynamic strIngredient6;
+  dynamic strIngredient7;
+  dynamic strIngredient8;
+  dynamic strIngredient9;
+  dynamic strIngredient10;
+  dynamic strIngredient11;
+  dynamic strIngredient12;
+  dynamic strIngredient13;
+  dynamic strIngredient14;
+  dynamic strIngredient15;
+  dynamic strMeasure1;
+  dynamic strMeasure2;
+  dynamic strMeasure3;
+  dynamic strMeasure4;
+  dynamic strMeasure5;
+  dynamic strMeasure6;
+  dynamic strMeasure7;
+  dynamic strMeasure8;
+  dynamic strMeasure9;
+  dynamic strMeasure10;
+  dynamic strMeasure11;
+  dynamic strMeasure12;
+  dynamic strMeasure13;
+  dynamic strMeasure14;
+  dynamic strMeasure15;
+
+  factory CocktailModel.fromJson(Map<String, dynamic> json) => CocktailModel(
+        idDrink: json["idDrink"],
+        strDrink: json["strDrink"],
+        strCategory: json["strCategory"],
+        strAlcoholic: json["strAlcoholic"],
+        strGlass: json["strGlass"],
+        strInstructions: json["strInstructions"],
+        strDrinkThumb: json["strDrinkThumb"],
+        strIngredient1: json["strIngredient1"],
+        strIngredient2: json["strIngredient2"],
+        strIngredient3: json["strIngredient3"],
+        strIngredient4: json["strIngredient4"],
+        strIngredient5: json["strIngredient5"],
+        strIngredient6: json["strIngredient6"],
+        strIngredient7: json["strIngredient7"],
+        strIngredient8: json["strIngredient8"],
+        strIngredient9: json["strIngredient9"],
+        strIngredient10: json["strIngredient10"],
+        strIngredient11: json["strIngredient11"],
+        strIngredient12: json["strIngredient12"],
+        strIngredient13: json["strIngredient13"],
+        strIngredient14: json["strIngredient14"],
+        strIngredient15: json["strIngredient15"],
+        strMeasure1: json["strMeasure1"],
+        strMeasure2: json["strMeasure2"],
+        strMeasure3: json["strMeasure3"],
+        strMeasure4: json["strMeasure4"],
+        strMeasure5: json["strMeasure5"],
+        strMeasure6: json["strMeasure6"],
+        strMeasure7: json["strMeasure7"],
+        strMeasure8: json["strMeasure8"],
+        strMeasure9: json["strMeasure9"],
+        strMeasure10: json["strMeasure10"],
+        strMeasure11: json["strMeasure11"],
+        strMeasure12: json["strMeasure12"],
+        strMeasure13: json["strMeasure13"],
+        strMeasure14: json["strMeasure14"],
+        strMeasure15: json["strMeasure15"],
+      );
+}
