61cec29a684adef8bea6aab44e4875c998e2cff8
DoomHopes <63532660+DoomHopes@users.noreply.github.com>
2021-09-21 11:55:22 +0300

    add list view

---
+++ lib/widgets/list_view_widget.dart
@@ -0,0 +1,24 @@
+import 'package:cocktail_app/model/cocktail_model.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ListViewWidget extends StatelessWidget {
+  List<CocktailModel> cocktails;
+
+  ListViewWidget(this.cocktails);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: cocktails.length,
+      itemBuilder: (context, index) {
+        return Card(
+          child: ListTile(
+            title: Text(cocktails[index].strDrink),
+            subtitle: Text(cocktails[index].strCategory),
+          ),
+        );
+      },
+    );
+  }
+}
