3a579d93d7144397e3e19f97379fc1af58c8051a
DoomHopes <63532660+DoomHopes@users.noreply.github.com>
2021-09-22 15:23:06 +0300

    add detail widget

---
+++ lib/widgets/detail_widget.dart
@@ -0,0 +1,14 @@
+import 'package:cocktail_app/model/cocktail_model.dart';
+import 'package:flutter/material.dart';
+
+class DetailWidget extends StatelessWidget {
+  DetailWidget(this.cocktailModel);
+
+  CocktailModel cocktailModel;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
