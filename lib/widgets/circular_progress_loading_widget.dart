12b6fb4152e778cbcd4aa242e15873a19f4e249b
DoomHopes <63532660+DoomHopes@users.noreply.github.com>
2021-09-22 11:01:39 +0300

    some change in circular progress widget

--- lib/widgets/circular_progress_loading_widget.dart
+++ lib/widgets/circular_progress_loading_widget.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
 
 class CircularProgressLoading extends StatelessWidget {
+  const CircularProgressLoading({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          CircularProgressIndicator(),
-          SizedBox(width: 40.0),
-          Text('Loading data ....'),
+          const CircularProgressIndicator(),
+          const SizedBox(width: 40.0),
+          const Text('Loading data ....'),
         ],
       ),
     );
